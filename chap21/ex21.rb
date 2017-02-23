a5b1bd2ed87cd59c3a708a3c4d62f48a5a61549c
Alex <alexanderjukes@gmail.com>
2017-02-23 10:57:33 +0000

    Thursday morning

---
+++ chap21/ex21.rb
@@ -0,0 +1,39 @@
+def add (a, b)
+  puts "ADDING #{a} + #{b}"
+  return a + b
+end
+
+def subtract(a, b)
+  puts "SUBTRACTING #{a} - #{b}"
+  return a - b
+end
+
+def multiply (a, b)
+  puts "MULTIPLYING #{a} * #{b}"
+  return a * b
+end
+
+def divide(a, b)
+  puts "DIVIDING #{a} / #{b}"
+  return a / b
+end
+
+
+puts "Let's do some maths with just functions!"
+
+age = add(30, 5)
+height = subtract(78, 4)
+weight = multiply(90, 2)
+iq = divide(100, 2)
+
+puts "Age: #{age}, Height: #{height}, Weight: #{weight}, IQ: #{iq}"
+
+puts "Here is a puzzle."
+what = multiply(age, add(iq, divide(weight, subtract(height, 3))))
+
+puts "That becomes #{what}. Can you do it by hand?"
+
+puts "Here's my formula:"
+formula = add(100, multiply(6, subtract(3, divide(20, 2))))
+
+puts "It's #{formula}"
